fcc8dd1edd569c3b3fc086030752ca870e53473a
Testfrst <TestNorthsec@gmail.com>
2025-05-11 14:26:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 80
-
-
+const express = require('express');
+const app = express();
+const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to your ASCPC web app....yes!')
-})
+  res.send('Welcome to your ASCPC web app....yes!');
+});
+
+// NEW: Route to print environment variables
+app.get('/env', (req, res) => {
+  res.json(process.env);
+});
 
 app.listen(port, () => {
-  console.log(Example app listening at http://localhost:${port})
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
